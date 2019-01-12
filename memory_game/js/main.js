cb3929548d9c40e68d61a881fe3d7e8364b6f5d2
owner <owner@mii-macbook-pro.attlocal.net>
2019-01-12 00:27:26 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,27 +1,30 @@
 console.log("Up and running!");
 
-var cards = [cardOne = "queen", cardTwo = "queen", cardThree = "king", cardFour = "king"];
-
-console.log("User flipped " + cardOne);
-console.log("user flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+//var cards = [cardOne = "queen", cardTwo = "queen", cardThree = "king", cardFour = "king"];
 
 var cardsInPlay = [];
 
-var cardOne = [];
+var cardOne = cards[0];
+//var cardOne = [];
+
+console.log("User flipped " + cardOne);
 
 cardsInPlay.push(cardOne);
 
 console.log();
 
-var cardTwo = [];
+var cardTwo = cards[2];
+
+console.log("User flipped " + cardTwo);
 
 cardsInPlay.push(cardTwo);
 
 console.log();
 
-if (cardsInPlay.length === 2); {
- 	else if(cardsInPlay[0] === cardsInPlay[1];)
- 	 	alert("You found a match!");
- 	 	 	else if(cardsInPlay[0 !== cardsInPlay[1];)
- 	 	 	 	alert("Sorry, try again.");
- 	 	 	 }
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1])
+		alert("You found a match!");
+	else 
+		alert("Sorry, no match!");
+}
